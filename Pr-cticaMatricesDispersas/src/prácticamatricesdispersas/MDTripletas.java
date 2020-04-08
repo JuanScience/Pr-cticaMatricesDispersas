a5cb5a96453a89c06569243d9a98499e8e314ede
user <user@DESKTOP-UN1B13Q>
2020-04-07 20:03:12 -0500

    Insertar sumando, Insertar cambiando (Incluye ajustar)

--- Pr-cticaMatricesDispersas/src/prácticamatricesdispersas/MDTripletas.java
+++ Pr-cticaMatricesDispersas/src/prácticamatricesdispersas/MDTripletas.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package prácticamatricesdispersas;
 
 /**
@@ -26,32 +22,108 @@ public class MDTripletas {
     
     //muestra la lista de tripletas
     public void mostrar(){
-        for(int i = 0; i < lista[0][2] + 1; i++){
-            System.out.println("[" + lista[i][0] + "]" + "[" + lista[i][1] + "]" + "[" + lista[i][2] + "]");
+        int digitos = this.findBigger(); //Encuentra el número más grande
+        for (int i = 0; i < this.getNDatos() + 1; i++) {
+            System.out.print("|");       
+            for (int j = 0; j < 3; j++) {
+                String espacios = " ";
+                for (int k = 1 ; k <= digitos - Float.toString(this.lista[i][j]).length(); k++) //Calcula el número de espacios para ordenar la impresión
+                    espacios = espacios + " ";
+                System.out.print(espacios + this.lista[i][j]); //Imprime el dato
+            }
+            System.out.println(" |");
         }
     }
     
-    //Almacena una tripleta, si encuentra un dato en la misma posición no permite
+    //Almacena una tripleta, si encuentra un dato en la misma posición lo suma
     public void almacenarTrip(int f, int c, float d){
-        int k = 1, j;
-        //Mientras recorre toda las filas de la tripleta, la fila de la matriz del dato que se va a ingresar es menor a la fila de la tripleta y el dato es diferente de cero
-        while(k < lista[0][2] + 1 && lista[k][0] <= f && lista[k][2] != 0){ 
-            k = k + 1; //Identifica la fila de la tripleta igual o anterior a la correspondiente fila de la matriz
+        int datos = getNDatos() + 1;
+        int k = 1;
+        //Mientras recorre toda las filas de la tripleta hasta la posición de almacenamiento
+        while(k < datos && (lista[k][0] < f) && lista[k][2] != 0){ 
+            k++; //Identifica la fila de inserción
         }
-        //Recorre las siguientes filas si hay más datos en la misma fila de la matriz
-        while (k < lista[0][2] + 1 && lista[k][0] <= f && lista[k][0] <= f) {
-            
+        while(k < datos && lista[k][0] == f && lista[k][1] < c && lista[k][2] != 0){ 
+            k++; //Identifica la fila de inserción
         }
+        if (k >= datos){
+            this.makeCopyPlusOne(k);
+            this.setDato(k, 0, f);
+            this.setDato(k, 1, c);
+            this.setDato(k, 2, d);
+        }else if (lista[k][0] == f && lista[k][1] == c) {
+            this.setDato(k, 2,  d + lista[k][2]);
+        }else if (lista[k][2] == 0){
+            this.setDato(k, 0, f);
+            this.setDato(k, 1, c);
+            this.setDato(k, 2, d);
+        }else{
+            this.makeCopyPlusOne(k);
+            this.setDato(k, 0, f);
+            this.setDato(k, 1, c);
+            this.setDato(k, 2, d);
+        }
+        this.redimensionarListaTrip();
+        System.out.println("Dato ingresado");
     }
     
-    //Inserta una tripleta, si encuentra un dato en la misma posición lo suma
+    //Inserta una tripleta, si encuentra un dato en la misma posición lo cambia
     public void insertarTrip(int f, int c, float d){
         
+        int datos = getNDatos() + 1;
+        int k = 1;
+        //Mientras recorre toda las filas de la tripleta hasta la posición de almacenamiento
+        while(k < datos && (lista[k][0] < f) && lista[k][2] != 0){ 
+            k++; //Identifica la fila de inserción
+        }
+        while(k < datos && lista[k][0] == f && lista[k][1] < c && lista[k][2] != 0){ 
+            k++; //Identifica la fila de inserción
+        }
+        if (k >= datos){
+            this.makeCopyPlusOne(k);
+            this.setDato(k, 0, f);
+            this.setDato(k, 1, c);
+            this.setDato(k, 2, d);
+        }else if (lista[k][0] == f && lista[k][1] == c) {
+            this.setDato(k, 2,  d);
+        }else if (lista[k][2] == 0){
+            this.setDato(k, 0, f);
+            this.setDato(k, 1, c);
+            this.setDato(k, 2, d);
+        }else{
+            this.makeCopyPlusOne(k);
+            this.setDato(k, 0, f);
+            this.setDato(k, 1, c);
+            this.setDato(k, 2, d);
+        }
+        this.redimensionarListaTrip();
+        System.out.println("Dato ingresado");
     }
     
-    //
+    //Elimina una fila de la tripleta si encuentra un cero
     public void redimensionarListaTrip(){
-        
+        int f = 1;
+        int datos = getNDatos() + 1;
+        while (f < datos && this.getDato(f, 2) != 0) {            
+            f++; //encuentra la fila donde hay cero
+        }
+        if (f < datos) {
+            float[][] listaAux = new float[datos - 1][3];
+            listaAux[0][0] = this.lista[0][0];
+            listaAux[0][1] = this.lista[0][1];
+            listaAux[0][2] = datos - 2;
+            for (int i = 1; i < f; i++) {
+                listaAux[i][0] = this.lista[i][0];
+                listaAux[i][1] = this.lista[i][1];
+                listaAux[i][2] = this.lista[i][2];
+            }
+            for (int i = f; i < datos - 1; i++) {
+                listaAux[i][0] = this.lista[i + 1][0];
+                listaAux[i][1] = this.lista[i + 1][1];
+                listaAux[i][2] = this.lista[i + 1][2];
+            }
+            this.lista = listaAux;
+        }
     }
     
     //Devuelve el número de filas de la matriz
@@ -85,8 +157,8 @@ public class MDTripletas {
     }
     
     //inserta un dato en una posición específica
-    public void setDato(int f, int c, int d){
-        
+    public void setDato(int f, int c, float d){
+        this.lista[f][c] = d;
     }
     
     //Suma las filas de la matriz
@@ -127,5 +199,37 @@ public class MDTripletas {
     //Hace una copia de una tripleta
     public MDTripletas hacerCopia(){
         return null;
+    }
+    
+    //Hace una copia de la tripleta aumentada en un espacio y corre los datos dejando libre la fila del parámetro
+    public void makeCopyPlusOne(int k){
+        int datos = this.getNDatos();
+        float[][] listaAux = new float[datos + 2][3]; //[filas 3][columnas]
+        listaAux[0][0] = this.lista[0][0];
+        listaAux[0][1] = this.lista[0][1];
+        listaAux[0][2] = this.lista[0][2] + 1;
+        for (int i = 1; i < k; i++) {
+            listaAux[i][0] = this.lista[i][0];
+            listaAux[i][1] = this.lista[i][1];
+            listaAux[i][2] = this.lista[i][2];
+        }
+        for (int i = k; i < datos + 1 ; i++) {
+            listaAux[i + 1][0] = this.lista[i][0];
+            listaAux[i + 1][1] = this.lista[i][1];
+            listaAux[i + 1][2] = this.lista[i][2];
+        }
+        this.lista = listaAux;
+    }   
+    
+    //Busca el número con mayor cantidad de dígitos
+    public int findBigger(){
+        int bigger = 0;
+        for (int i = 0; i < this.getNDatos(); i++) {
+            for (int j = 0; j < 3; j++) {
+                if (bigger < Float.toString(this.lista[i][j]).length())
+                    bigger = Float.toString(this.lista[i][j]).length();
+            }
+        }
+        return bigger;
     }
 }
